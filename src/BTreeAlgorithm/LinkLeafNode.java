303ac40a0a67d6ac3836fbc727cd3922305c5ff0
Jinghua ZHU <zjhzhush@cn.ibm.com>
2016-09-01 21:31:47 +0800

    add recursion function for linking leaf nodes

--- src/BTreeAlgorithm/LinkLeafNode.java
+++ src/BTreeAlgorithm/LinkLeafNode.java
@@ -7,15 +7,22 @@ import BinaryTree.BTree;
 // Link all leaf nodes from left to right 
 // by using their right child.
 public class LinkLeafNode {
+	class InnerLinkedList {
+		public BNode head = null;
+		public BNode tail = null;
+	}
+	
 	public static void main(String[] args) {
 		String[] init = new String[]{"1", "[", "2", "[", "4", ",", "5", "]", ",", "3", "[", "6", ",", "]", "]"};	
 		BTree bt = new BTree(init);
-		// BNode n = linkNode(null);
-		BNode n = linkNode(bt.getRoot());
-		printLink(n);
+		// BNode n = linkNodeN(null);
+		//BNode n = linkNodeN(bt.getRoot());
+		InnerLinkedList ll = new LinkLeafNode().new InnerLinkedList();
+		linkNode(bt.getRoot(), ll);
+		printLink(ll.head);
 	}
 	
-	public static BNode linkNode(BNode root) {
+	public static BNode linkNodeN(BNode root) {
 		Stack<BNode> st = new Stack<BNode>();
 		BNode start = null, cur = null, n = root;
 		
@@ -41,6 +48,27 @@ public class LinkLeafNode {
 		} // while
 		
 		return start;
+	}
+	
+	public static void linkNode(BNode root, InnerLinkedList ll) {
+		if(root != null) {
+			if(root.isLeaf()) {
+				if(ll.head == null) {
+					ll.head = root;
+				} else {
+					ll.tail.setRight(root);
+				}
+				ll.tail = root;
+			}
+			
+			if(root.getLeft() != null) {
+				linkNode(root.getLeft(), ll);
+			}
+			
+			if(root.getRight() != null) {
+				linkNode(root.getRight(), ll);
+			}
+		}
 	}
 	
 	public static void printLink(BNode start) {
