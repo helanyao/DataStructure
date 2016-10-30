3d1b71aea3a86d6b9f87cf0665b118dd7131671d
jinghzhu <jhzhu@outlook.com>
2016-10-30 17:04:48 +0800

    rename node to ListNode

--- src/SingleLinkedList/LinkedListTest.java
+++ src/SingleLinkedList/LinkedListTest.java
@@ -3,7 +3,7 @@ package SingleLinkedList;
 public class LinkedListTest {
 
 	public static void main(String[] args) {
-		Node n = new Node(0);
+		ListNode n = new ListNode(0);
 		LinkedList ll = new LinkedList(n);
 		ll.add(1);
 		ll.add(2);
